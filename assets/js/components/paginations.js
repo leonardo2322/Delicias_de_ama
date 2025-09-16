e202e397526fc59af98b9babbc0497a295ed7f42
leonardo2322 <joys23services@gmail.com>
2025-09-15 23:07:02 -0500

    subiendo proyecto

---
+++ assets/js/components/paginations.js
@@ -0,0 +1,141 @@
+import { numberToCurrency } from "../helpers/numberCurrency.js";
+import { db } from "./products.js";
+
+export const pagination = () => {
+  const product = db.methods.getAll();
+
+  const tipoDatos = [
+    "caseros_bandejas",
+    "planchas_saludabes",
+    "porciones_menu",
+  ];
+  let options = JSON.parse(localStorage.getItem("options"));
+  const itemsPerPage = 5;
+  let currentPage = 1;
+  const productLength = renderItems(product, currentPage, options.p)[0][
+    options.p
+  ].length;
+
+  function showPage(pageNumber, product) {
+    currentPage = pageNumber;
+    const startIndex = (pageNumber - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const tipoDatos = [
+      "planchas_saludabes",
+      "caseros_bandejas",
+      "porciones_menu",
+    ];
+
+    const dataContainer = document.getElementById("product-container");
+    dataContainer.innerHTML = "";
+    let html = "";
+
+    tipoDatos.forEach((tipo) => {
+      if (product[0].hasOwnProperty(tipo)) {
+        const datos = product[0][tipo].slice(startIndex, endIndex);
+
+        datos.forEach((item) => {
+          const texto = item.disponible ? "Disponible" : "PorEncargo";
+          const color = item.disponible ? "green" : "red";
+          html += `
+                  <div class="card" id="card" data-id="${item.id}">
+                      <div class="card-img">
+                          <div class="img">
+                              <img src="${item.imagen}" alt="${item.titulo}">
+                          </div>
+                      </div>
+                      <div class="card-title" ">${item.titulo}</div>
+                      <div class="card-subtitle" >${
+                        item.detalles
+                      } <span style="color: ${color};">${texto}</span></div>
+                      <hr class="card-divider">
+                      <div class="card-footer">
+                          <div class="card-price">${numberToCurrency(
+                            item.precio
+                          )}</div>
+                          <button class="card-btn addCarts" data-id="${
+                            item.id
+                          }">
+                              <i class="fa-solid fa-cart-plus addCarts" data-id="${
+                                item.id
+                              }"></i>
+                          </button>
+                          <button class="card-btn eyes" data-id="${item.id}">
+                              <i class="eyes fa-solid fa-eye" data-id="${
+                                item.id
+                              }"></i>
+                          </button>
+                      </div>
+                  </div>
+              `;
+        });
+      }
+    });
+
+    dataContainer.innerHTML = html;
+    updatePaginationButtons();
+  }
+
+  function renderItems(prod, current, elmnt) {
+    const filteredItems = prod.filter((item) => {
+      // Filtrar por la propiedad `elmnt` según `options.p`
+      return (
+        item.hasOwnProperty(elmnt) && Object.keys(item).includes(options.p)
+      );
+    });
+
+    return filteredItems;
+  }
+
+  function generatePaginationButtons(data) {
+    const paginationContainer = document.getElementById("number-page");
+    paginationContainer.innerHTML = "";
+
+    tipoDatos.forEach((tipo) => {
+      if (data[0].hasOwnProperty(tipo)) {
+        const tipoData = data[0][tipo];
+        for (let i = 1; i <= Math.ceil(tipoData.length / itemsPerPage); i++) {
+          const button = document.createElement("button");
+          button.classList.add("counter-page");
+          button.textContent = i;
+          button.addEventListener("click", () => showPage(i, data));
+          paginationContainer.appendChild(button);
+        }
+      }
+    });
+  }
+  function updatePaginationButtons() {
+    const buttons = document.querySelectorAll("#number-page .counter-page");
+    buttons.forEach((button, index) => {
+      if (index + 1 === currentPage) {
+        button.classList.add("current");
+      } else {
+        button.classList.remove("current");
+      }
+    });
+  }
+
+  function goToNextPage() {
+    const totalPages = Math.ceil(productLength / itemsPerPage);
+    if (currentPage < totalPages) {
+      showPage(currentPage + 1, renderItems(product, currentPage, options.p));
+    }
+  }
+
+  function goToPrevPage() {
+    if (currentPage > 1) {
+      showPage(currentPage - 1, renderItems(product, currentPage, options.p));
+    }
+  }
+
+  function initializePagination() {
+    generatePaginationButtons(renderItems(product, currentPage, options.p));
+    showPage(currentPage, renderItems(product, currentPage, options.p));
+    // showPage(currentPage, product);
+
+    document.getElementById("prev").addEventListener("click", goToPrevPage);
+    document.getElementById("next").addEventListener("click", goToNextPage);
+  }
+
+  initializePagination();
+};
---
+++ assets/js/components/sendMessageInfo.js
@@ -0,0 +1,55 @@
+import { cart, renderCart } from "./cart.js";
+
+export const sendMessage = () => {
+  const send = document.getElementById("send");
+
+  send.addEventListener("click", () => {
+    const data = leerLocalStorage();
+    const totalCart = data.totalCart;
+    const products = data.productsCart;
+    const EditPlates = document.getElementById("textoEdit");
+    const textEdit = EditPlates.value ? EditPlates.value : "Sin Ediciones";
+    const mediosPago =
+      "Pago movil Provincial: 04128757762, C.I: 28.153.152 💵 ";
+    let textoProducto = "";
+    let counterProduct = Math.round(Math.random() * 2557);
+
+    products.map((item) => {
+      const titulo = item.titulo;
+      const precio = item.precio;
+      const cantidad = item.quantity;
+
+      textoProducto += `Producto: ${titulo},  cantidad: ${cantidad},  Precio: $${precio} \n`;
+
+      counterProduct++;
+    });
+
+    let mensage = `Orden N°: ${counterProduct} \n \n Hola Te Saluda Palerossi👋 \n \n Tipo de servicio: Compra🛒\n \nMedios de Pago: ${mediosPago}💰💸 \n \n Descripcion: 📝\n ${textoProducto} \n \n Total Compra: $${totalCart}💵 \n \nLa Orden tiene las Siguientes Ediciones: \n \n${textEdit}📝 \n \n Gracias por comprar con nosotros enseguida te atenderemos🕦.`;
+
+    const tlf = "573502117928";
+
+    const enlaceWhatsapp =
+      `https://wa.me/${tlf}/?text=` + encodeURIComponent(mensage);
+
+    window.open(enlaceWhatsapp, "_blank");
+
+    cart.items = [];
+    renderCart();
+  });
+};
+
+function leerLocalStorage() {
+  let dataStorage = cart.methods.getAll();
+  const totalCart = cart.methods.getTotal();
+  let productsCart = [];
+
+  dataStorage.map((item) => {
+    productsCart.push(item);
+  });
+
+  return {
+    productsCart,
+    totalCart,
+    dataStorage,
+  };
+}
